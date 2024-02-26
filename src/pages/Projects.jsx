103015ee1bfa57551b35d18d1d1092066cccd854
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-02-26 14:09:54 +0000

    Finished the projects page.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Card from "../components/Card";
+import "../styles/Projects.css";
 
 function Projects() {
   return (
     <div>
+      <h1 className="projects__title">Projects</h1>
       <Card />
     </div>
   );
