69b79cc8e5b4bae3ba3bd86f7019e93ed30d7372
Danté Kellman-Thompson <dante.kellman@icloud.com>
2025-08-07 21:37:00 +0100

    added to the code.

--- src/pages/DevelopmentProjects.jsx
+++ src/pages/DevelopmentProjects.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+// In case the data in the json file keeps growing. Want to ensure it performs well.
 const DevProjectCard = React.lazy(() => {
   return import("../components/DevProjectCard");
 });
 //Gets code from the card.
+// Using React.Suspense in case it takes long to pull the data in.
 export default function developmentProjects() {
   return (
     <React.Suspense fallback={<h2>Loading...</h2>}>
